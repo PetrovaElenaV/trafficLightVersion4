02eaa8ee885c1039ac47c3cd16cbbbc2db43381b
Elena Petrova <e_v_b@mail.ru>
2023-07-26 21:32:45 +0300

    Initial Commit

---
+++ trafficLightVersion4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLightVersion4
+//
+//  Created by Elena Petrova on 26.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
