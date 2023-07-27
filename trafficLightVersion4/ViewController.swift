949a9d144211a3eb519d855588d7056c444f25de
Elena Petrova <e_v_b@mail.ru>
2023-07-27 17:02:34 +0300

    Светофор. минимум задач

--- trafficLightVersion4/ViewController.swift
+++ trafficLightVersion4/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLabel: UIView!
+    @IBOutlet var yellowLabel: UIView!
+    @IBOutlet var greenLabel: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.layer.cornerRadius = redLabel.frame.width / 2
+        yellowLabel.layer.cornerRadius = yellowLabel.frame.width / 2
+        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
+        
+        redLabel.alpha = 0.3
+        yellowLabel.alpha = 0.3
+        greenLabel.alpha = 0.3
     }
-
-
+    
+    @IBAction func pushStartButton() {
+   
+        if redLabel.alpha != 1 && yellowLabel.alpha != 1 && greenLabel.alpha != 1 {
+            startButton.setTitle("NEXT", for: .normal)
+            redLabel.alpha = 1
+            yellowLabel.alpha = 0.3
+            greenLabel.alpha = 0.3
+        } else if redLabel.alpha == 1 && yellowLabel.alpha != 1 && greenLabel.alpha != 1 {
+            startButton.setTitle("NEXT", for: .normal)
+            redLabel.alpha = 0.3
+            yellowLabel.alpha = 1
+            greenLabel.alpha = 0.3
+        } else if redLabel.alpha != 1 && yellowLabel.alpha == 1 && greenLabel.alpha != 1{
+            startButton.setTitle("NEXT", for: .normal)
+            redLabel.alpha = 0.3
+            yellowLabel.alpha = 0.3
+            greenLabel.alpha = 1
+        } else {
+            startButton.setTitle("NEXT", for: .normal)
+            redLabel.alpha = 1
+            yellowLabel.alpha = 0.3
+            greenLabel.alpha = 0.3
+        }
+        
+    }
+    
+    
+    
+    
 }
 
